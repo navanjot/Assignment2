2b4ce39e592618c5d7be4f69e25c9db2e37fcd50
Navanjot Kaur <navanjotkaur@gmail.com>
2017-10-14 22:45:41 -0400

    Initial commit

---
+++ WebForm1.aspx
@@ -0,0 +1,116 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="Assignment2.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body style="height: 481px">
+    <form id="form1" runat="server">
+    <div style="height: 1394px">
+    
+        <header style="text-align:center">
+            Course: PROG37721<br />
+            Assignment #: 2<br />
+            Name: Navanjot Kaur<br />
+            Id: 991413777<br />
+            Instructor’s name: Syed Tanbeer<br />
+        </header>
+        <br />
+        <br />
+        <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" style="margin-left: 63px" Text="Click to Insert Items" />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <br />
+        <asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="False" CellPadding="4" DataKeyNames="ItemId" DataSourceID="SqlDataSource1" ForeColor="#333333" GridLines="None">
+            <AlternatingRowStyle BackColor="White" ForeColor="#284775" />
+            <Columns>
+                <asp:BoundField DataField="ItemId" HeaderText="ItemId" ReadOnly="True" SortExpression="ItemId" />
+                <asp:BoundField DataField="Name" HeaderText="Name" SortExpression="Name" />
+                <asp:BoundField DataField="Category" HeaderText="Category" SortExpression="Category" />
+                <asp:BoundField DataField="UnitPrice" HeaderText="UnitPrice" SortExpression="UnitPrice" />
+            </Columns>
+            <EditRowStyle BackColor="#999999" />
+            <FooterStyle BackColor="#5D7B9D" Font-Bold="True" ForeColor="White" />
+            <HeaderStyle BackColor="#5D7B9D" Font-Bold="True" ForeColor="White" />
+            <PagerStyle BackColor="#284775" ForeColor="White" HorizontalAlign="Center" />
+            <RowStyle BackColor="#F7F6F3" ForeColor="#333333" />
+            <SelectedRowStyle BackColor="#E2DED6" Font-Bold="True" ForeColor="#333333" />
+            <SortedAscendingCellStyle BackColor="#E9E7E2" />
+            <SortedAscendingHeaderStyle BackColor="#506C8C" />
+            <SortedDescendingCellStyle BackColor="#FFFDF8" />
+            <SortedDescendingHeaderStyle BackColor="#6F8DAE" />
+        </asp:GridView>
+        <asp:SqlDataSource ID="SqlDataSource1" runat="server" ConnectionString="<%$ ConnectionStrings:ConnectionString %>" SelectCommand="SELECT * FROM [Items]"></asp:SqlDataSource>
+        <br />
+        <asp:Button ID="Button2" runat="server" OnClick="Button2_Click" Text="Click to insert Transactions" />
+        <asp:GridView ID="GridView2" runat="server" AutoGenerateColumns="False" CellPadding="4" DataKeyNames="TranscationId" DataSourceID="SqlDataSource2" ForeColor="#333333" GridLines="None">
+            <AlternatingRowStyle BackColor="White" ForeColor="#284775" />
+            <Columns>
+                <asp:BoundField DataField="TranscationId" HeaderText="TranscationId" ReadOnly="True" SortExpression="TranscationId" />
+                <asp:BoundField DataField="ItemId" HeaderText="ItemId" SortExpression="ItemId" />
+                <asp:BoundField DataField="quality" HeaderText="quality" SortExpression="quality" />
+            </Columns>
+            <EditRowStyle BackColor="#999999" />
+            <FooterStyle BackColor="#5D7B9D" Font-Bold="True" ForeColor="White" />
+            <HeaderStyle BackColor="#5D7B9D" Font-Bold="True" ForeColor="White" />
+            <PagerStyle BackColor="#284775" ForeColor="White" HorizontalAlign="Center" />
+            <RowStyle BackColor="#F7F6F3" ForeColor="#333333" />
+            <SelectedRowStyle BackColor="#E2DED6" Font-Bold="True" ForeColor="#333333" />
+            <SortedAscendingCellStyle BackColor="#E9E7E2" />
+            <SortedAscendingHeaderStyle BackColor="#506C8C" />
+            <SortedDescendingCellStyle BackColor="#FFFDF8" />
+            <SortedDescendingHeaderStyle BackColor="#6F8DAE" />
+        </asp:GridView>
+        <asp:SqlDataSource ID="SqlDataSource2" runat="server" ConnectionString="<%$ ConnectionStrings:ConnectionString %>" SelectCommand="SELECT * FROM [Transcation]"></asp:SqlDataSource>
+        <br />
+        <asp:DropDownList ID="DropDownList1" runat="server" OnSelectedIndexChanged="DropDownList1_SelectedIndexChanged">
+        </asp:DropDownList>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:Button ID="Button3" runat="server" OnClick="Button3_Click" Text="Button" />
+&nbsp;<asp:GridView ID="GridView3" runat="server" CellPadding="4" ForeColor="#333333" GridLines="None">
+            <AlternatingRowStyle BackColor="White" ForeColor="#284775" />
+            <EditRowStyle BackColor="#999999" />
+            <FooterStyle BackColor="#5D7B9D" Font-Bold="True" ForeColor="White" />
+            <HeaderStyle BackColor="#5D7B9D" Font-Bold="True" ForeColor="White" />
+            <PagerStyle BackColor="#284775" ForeColor="White" HorizontalAlign="Center" />
+            <RowStyle BackColor="#F7F6F3" ForeColor="#333333" />
+            <SelectedRowStyle BackColor="#E2DED6" Font-Bold="True" ForeColor="#333333" />
+            <SortedAscendingCellStyle BackColor="#E9E7E2" />
+            <SortedAscendingHeaderStyle BackColor="#506C8C" />
+            <SortedDescendingCellStyle BackColor="#FFFDF8" />
+            <SortedDescendingHeaderStyle BackColor="#6F8DAE" />
+        </asp:GridView>
+        <br />
+        <br />
+        <br />
+        <asp:Label ID="Label1" runat="server" BackColor="#FF9999" BorderStyle="Outset" Text="Items that was not Purchased"></asp:Label>
+&nbsp;&nbsp;
+        <asp:Button ID="Button4" runat="server" BackColor="#FF9999" BorderStyle="Outset" Height="22px" OnClick="Button4_Click" Text="Click !" Width="50px" />
+        <br />
+        <asp:GridView ID="GridView4" runat="server" BackColor="#FF9999">
+        </asp:GridView>
+        <br />
+&nbsp;<asp:Label ID="Label3" runat="server" BackColor="#CCCCFF" BorderStyle="Outset" Text="Total Cost of all transactions"></asp:Label>
+&nbsp;<asp:Button ID="Button5" runat="server" BackColor="#CCCCFF" BorderStyle="Outset" Height="20px" OnClick="Button5_Click" Text="Click !" Width="50px" />
+&nbsp;
+        <br />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <br />
+&nbsp;<asp:Label ID="Label2" runat="server" BackColor="#CCCCFF" BorderStyle="Outset"></asp:Label>
+        <br />
+        <br />
+        <br />
+        <asp:Label ID="Label4" runat="server" BackColor="#009999" BorderStyle="Outset" Text="Items purchased more than 3 in quantity"></asp:Label>
+&nbsp;
+        <asp:Button ID="Button6" runat="server" BackColor="#009999" BorderStyle="Outset" Height="18px" OnClick="Button6_Click1" Text="Click !" Width="54px" />
+        <br />
+        <asp:GridView ID="GridView5" runat="server" BackColor="#009999">
+        </asp:GridView>
+        <br />
+&nbsp;&nbsp;&nbsp;&nbsp;
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ WebForm1.aspx.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.IO;
+namespace Assignment2
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        SqlConnection conn;
+        SqlCommand cmd;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            conn = new SqlConnection(ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString);
+
+            conn.Open();
+            if (!IsPostBack)
+            {
+                DropDownList1.DataSource = FetchCategory();
+                DropDownList1.DataBind();
+                DropDownList1.Items.Insert(0, new ListItem("Select a category", "-1"));
+                 
+            }
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            StreamReader sr = new StreamReader(File.Open("C:\\COURSES\\PROG37721\\VisualStudio\\Assignment2\\Items.txt", FileMode.Open));
+
+            conn = new SqlConnection(ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString);
+            {
+                conn.Open();
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    string[] parts = line.Split(new char[] { ' ' });
+
+                    string insert = "Insert into Items values(@ItemId, @Name, @Category, @UnitPrice)";
+                    cmd = new SqlCommand(insert, conn);
+                    cmd.Parameters.AddWithValue("@ItemId", parts[0]);
+                    cmd.Parameters.AddWithValue("@Name", parts[1]);
+                    cmd.Parameters.AddWithValue("@Category", parts[2]);
+                    cmd.Parameters.AddWithValue("@UnitPrice", parts[3]);
+                    cmd.ExecuteNonQuery();
+
+                }
+                conn.Close();
+
+            }
+            Response.Redirect("WebForm1.aspx");
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            StreamReader sr = new StreamReader(File.Open("C:\\COURSES\\PROG37721\\VisualStudio\\Assignment2\\Transactions.txt", FileMode.Open));
+
+            conn = new SqlConnection(ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString);
+            {
+                conn.Open();
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    string[] parts = line.Split(new char[] { ' ' });
+
+                    string insert = "Insert into Transcation values(@Transcation, @ItemId, @quality)";
+                    cmd = new SqlCommand(insert, conn);
+                    cmd.Parameters.AddWithValue("@Transcation", parts[0]);
+                    cmd.Parameters.AddWithValue("@ItemId", parts[1]);
+                    cmd.Parameters.AddWithValue("@quality", parts[2]);
+                    cmd.ExecuteNonQuery();
+
+                }
+                conn.Close();
+
+            }
+            Response.Redirect("WebForm1.aspx");
+
+
+        }
+
+        protected void DropDownList1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+     
+                FetchItem(DropDownList1.SelectedValue);
+
+         }
+        private DataSet FetchCategory()
+        {
+            conn = new SqlConnection(ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString);
+            string query = "Select DISTINCT Category from Items";
+            SqlDataAdapter da = new SqlDataAdapter(query, conn);
+            DataSet ds = new DataSet();
+            DropDownList1.DataTextField = "Category";
+            da.Fill(ds);
+            return ds;
+        }
+        private void FetchItem(string tempCategory)
+        {
+
+            conn = new SqlConnection(ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString);
+            conn.Open();
+            string query = "Insert Into CategoryTotal(ItemId,Item,Amount) Select t.ItemId,t.Name ,t.UnitPrice*Sum(quality) AS Amount from Items t INNER JOIN Transcation r ON t.ItemId = r.ItemId Where Category = @category Group BY t.ItemId,t.Name,t.UnitPrice";
+            SqlCommand cmd = new SqlCommand(query, conn);
+            cmd.Parameters.AddWithValue("@Category", tempCategory);
+            cmd.ExecuteNonQuery();
+
+        }
+
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+
+            //Response.Write(DropDownList1.SelectedIndex);
+            Response.Write(DropDownList1.SelectedIndex);
+            conn = new SqlConnection(ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString);
+            conn.Open();
+            string query = "Select * from CategoryTotal";
+            SqlCommand cmd = new SqlCommand(query, conn);
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+            GridView3.DataSource = ds;
+            GridView3.DataBind();
+            conn.Close();
+        }
+
+        protected void Button4_Click(object sender, EventArgs e)
+        {
+            conn = new SqlConnection(ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString);
+            conn.Open();
+            string query = "Select t.ItemId, Name, Category, UnitPrice From Items t Where Not Exists (Select ItemId From Transcation r Where t.ItemId = r.ItemId) ";
+            SqlCommand cmd = new SqlCommand(query, conn);
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+            GridView4.DataSource = ds;
+            GridView4.DataBind();
+            conn.Close();
+        }
+
+        protected void Button5_Click(object sender, EventArgs e)
+        {
+            conn = new SqlConnection(ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString);
+            conn.Open();
+            string query = "Select t.UnitPrice*Sum(quality) AS Total_Cost from Items t INNER JOIN Transcation r ON t.ItemId = r.ItemId Group BY t.UnitPrice";
+            double Total = 0.0;
+            SqlCommand cmd = new SqlCommand(query, conn);
+            using (SqlDataReader r = cmd.ExecuteReader())
+            {
+                while (r.Read())
+                {
+                    Total += Convert.ToDouble(r["Total_Cost"]);
+                }
+            }
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            Label2.Text = "The Total cost of all Transactions are " + Total;
+            conn.Close();
+        }
+
+        protected void Button6_Click(object sender, EventArgs e)
+        {
+            conn = new SqlConnection(ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString);
+            conn.Open();
+            string query = "Select DISTINCT Name, Category From Items t Inner Join Transcation r ON t.ItemId = r.ItemId Where quality > 3 ";
+            SqlCommand cmd = new SqlCommand(query, conn);
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+            GridView5.DataSource = ds;
+            GridView5.DataBind();
+            conn.Close();
+        }
+
+        protected void Button6_Click1(object sender, EventArgs e)
+        {
+            conn = new SqlConnection(ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString);
+            conn.Open();
+            string query = "Select DISTINCT Name, Category From Items t Inner Join Transcation r ON t.ItemId = r.ItemId Where quality > 3 ";
+            SqlCommand cmd = new SqlCommand(query, conn);
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+            GridView5.DataSource = ds;
+            GridView5.DataBind();
+            conn.Close();
+        }
+
+    }
+}
